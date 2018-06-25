c0fb61a20c02cdb1e3bc04bbcbc1c7f4a8ae0061
Angelina Velinska <angelina.velinska@coremedia.com>
2018-06-25 19:47:04 +0200

    parameterized HOST, TENANT and SITE_ID

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import ErrorBoundary from './containers/ErrorBoundary';
 import theme from './styles/themes/default';
 
 const elements = document.getElementsByTagName('cm-fragment');
+const HOST = process.env.REACT_APP_API_HOST || 'http://127.0.0.1:8080';
+const TENANT_ID = process.env.REACT_APP_TENANT_ID;
+const SITE_ID = process.env.REACT_APP_SITE_ID;
 
 const parseParams = (params: string): Object | void => {
   try {
@@ -26,9 +29,9 @@ for (const item of elements) {
     <ThemeProvider theme={theme}>
       <ErrorBoundary>
         <Fetch
-          host="http://127.0.0.1:8080"
-          tenantId="coremedia"
-          siteId="caassiopeia-en-DE"
+          host={HOST}
+          tenantId={TENANT_ID}
+          siteId={SITE_ID}
           fragmentType={show}
           contentId={id}
         >
